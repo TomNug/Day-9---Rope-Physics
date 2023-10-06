5c17e9539700967ce373bb5cb4d1e2b448afec27
t.nugent <t.nugent@hotmail.co.uk>
2023-10-06 14:38:09 +0100

    Add project files.

---
+++ Day 9 - Rope Physics/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text; // For stringbuilder
+
+class Program
+{
+    
+
+
+
+    public static void DisplayGrid(char[,] grid)
+    {
+        for(int row=0; row<grid.GetLength(0); row++)
+        {
+            StringBuilder sb = new StringBuilder();
+            
+            for (int col=0; col<grid.GetLength(1); col++)
+            {
+                sb.Append(grid[row,col]);
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        char[,] grid =  {
+                    {'.','.','.','.','.','.'},
+                    {'.','.','.','.','.','.'},
+                    {'.','.','.','.','.','.'},
+                    {'.','.','.','.','.','.'},
+                    {'.','.','.','.','.','.'}
+        };
+        DisplayGrid(grid);
+    }
+}
