21fbf083829c4575798bbe3bf1d848700bdf1cfd
t.nugent <t.nugent@hotmail.co.uk>
2023-10-07 15:02:27 +0100

    Reading text file to determine instructions (not yet implemented). Updated Follow method to work with tuples to make it easier to store coordinates elsewhere in the program.

--- Day 9 - Rope Physics/Program.cs
+++ Day 9 - Rope Physics/Program.cs
@@ -2,46 +2,42 @@
 
 class Program
 {
-    public static (int, int) Follow(int headRow, int headCol, int tailRow, int tailCol)
+    public static (int, int) Follow((int,int) headRowCol, (int, int) tailRowCol)
     {
         // Assume no movement
-        int newRow = tailRow;
-        int newCol = tailCol;
+        (int,int) newRowCol = tailRowCol;
 
         // Calculate distances between head and tail
-        int rowDiff = headRow - tailRow; 
-        int colDiff = headCol - tailCol;
+        int rowDiff = headRowCol.Item1 - tailRowCol.Item1; 
+        int colDiff = headRowCol.Item2 - tailRowCol.Item2;
 
         // Head is far enough away to move the tail
-        if (rowDiff > 1 || colDiff > 1)
+        if (Math.Abs(rowDiff) > 1 || Math.Abs(colDiff) > 1)
         {
             // If in same row or column
             if (rowDiff == 0 || colDiff == 0)
             {
                 // Moves one closer to head
-                newCol = (headCol + tailCol) / 2;
-                newRow = (headRow + tailRow) / 2;
+                newRowCol.Item2 = (headRowCol.Item2 + tailRowCol.Item2) / 2;
+                newRowCol.Item1 = (headRowCol.Item1 + tailRowCol.Item1) / 2;
             }
             // Difference in row and column
             else
             {
                 if (Math.Abs(rowDiff) == 1)
                 {
-                    newRow += rowDiff;
-                    newCol += colDiff / 2;
+                    newRowCol.Item1 += rowDiff;
+                    newRowCol.Item2 += colDiff / 2;
                 }
                 else
                 {
-                    newRow += rowDiff / 2;
-                    newCol += colDiff;
+                    newRowCol.Item1 += rowDiff / 2;
+                    newRowCol.Item2 += colDiff;
                 }
             }
         }
-        return (newRow, newCol);
+        return newRowCol;
     }
-
-
-
     public static void DisplayGrid(char[,] grid)
     {
         for(int row=0; row<grid.GetLength(0); row++)
@@ -55,61 +51,59 @@ class Program
             Console.WriteLine(sb.ToString());
         }
     }
-
     public static void Tests()
     {
         // Tests
 
         // Down 
-        (int, int) result = Follow(4, 2, 2, 2);
+        (int, int) result = Follow((4, 2), (2, 2));
         Console.WriteLine(String.Format("D: H(4,2), T(2,2) -> ({0},{1}) <(3,2)>", result.Item1, result.Item2));
 
         // Up
-        result = Follow(0, 2, 2, 2);
+        result = Follow((0, 2), (2, 2));
         Console.WriteLine(String.Format("U: H(0,2), T(2,2) -> ({0},{1}) <(1,2)>", result.Item1, result.Item2));
 
         // Left
-        result = Follow(2, 0, 2, 2);
+        result = Follow((2, 0), (2, 2));
         Console.WriteLine(String.Format("L: H(2,0), T(2,2) -> ({0},{1}) <(2,1)>", result.Item1, result.Item2));
 
         // Right
-        result = Follow(2, 4, 2, 2);
+        result = Follow((2, 4), (2, 2));
         Console.WriteLine(String.Format("R: H(2,4), T(2,2) -> ({0},{1}) <(2,3)>", result.Item1, result.Item2));
 
         // NNE
-        result = Follow(0, 3, 2, 2);
+        result = Follow((0, 3), (2, 2));
         Console.WriteLine(String.Format("NNE: H(0,3), T(2,2) -> ({0},{1}) <(1,3)>", result.Item1, result.Item2));
 
         // ENE
-        result = Follow(1, 4, 2, 2);
+        result = Follow((1, 4), (2, 2));
         Console.WriteLine(String.Format("ENE: H(1,4), T(2,2) -> ({0},{1}) <(1,3)>", result.Item1, result.Item2));
 
         // ESE
-        result = Follow(3, 4, 2, 2);
+        result = Follow((3, 4), (2, 2));
         Console.WriteLine(String.Format("ESE: H(3,4), T(2,2) -> ({0},{1}) <(3,3)>", result.Item1, result.Item2));
 
         // SSE
-        result = Follow(4, 3, 2, 2);
+        result = Follow((4, 3), (2, 2));
         Console.WriteLine(String.Format("SSE: H(4,3), T(2,2) -> ({0},{1}) <(3,3)>", result.Item1, result.Item2));
 
         // SSW
-        result = Follow(4, 1, 2, 2);
+        result = Follow((4, 1), (2, 2));
         Console.WriteLine(String.Format("SSW: H(4,1), T(2,2) -> ({0},{1}) <(3,1)>", result.Item1, result.Item2));
 
         // WSW
-        result = Follow(3, 0, 2, 2);
+        result = Follow((3, 0), (2, 2));
         Console.WriteLine(String.Format("WSW: H(3,0), T(2,2) -> ({0},{1}) <(3,1)>", result.Item1, result.Item2));
 
         // WNW
-        result = Follow(1, 0, 2, 2);
+        result = Follow((1, 0), (2, 2));
         Console.WriteLine(String.Format("WNW: H(1,0), T(2,2) -> ({0},{1}) <(1,1)>", result.Item1, result.Item2));
 
         // NNW
-        result = Follow(0, 1, 2, 2);
+        result = Follow((0, 1), (2, 2));
         Console.WriteLine(String.Format("NNW: H(0,1), T(2,2) -> ({0},{1}) <(1,1)>", result.Item1, result.Item2));
 
     }
-
     public static void Main(string[] args)
     {
         char[,] grid =  {
@@ -120,9 +114,22 @@ class Program
                     {'.','.','.','.','.','.'}
         };
         DisplayGrid(grid);
-
         Tests();
-        
+
+        string[] movs = System.IO.File.ReadAllLines(@"C:\Users\Tom\Documents\Advent\Day 9 - Rope Physics\Day 9 - Rope Physics\data_test.txt");
+        //string[] rows = System.IO.File.ReadAllLines(@"C:\Users\Tom\Documents\Advent\Day 8 - Tree Heights\Day 8 - Tree Heights\data_full.txt");
+
+
+        (int, int) headRowCol = (grid.GetLength(0), 0);
+        (int, int) tailRowCol = (grid.GetLength(0), 0);
+
+        foreach(string mov in movs)
+        {
+            string[] splitMov = mov.Split(" ");
+            string dir = splitMov[0];
+            int dist = int.Parse(splitMov[1]);
+        }
+
     }
 
 }
